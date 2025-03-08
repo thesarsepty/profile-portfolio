33336eb4ea468b6e006075ae1748b210615ecf3d
thesarsepty <thesarsepty@gmail.com>
2025-03-08 21:46:15 +0700

    homepage done

--- src/app/Data/data.ts
+++ src/app/Data/data.ts
@@ -3,7 +3,7 @@ export const BaseInfo = {
   position: "Full Stack Web Developer",
   description:
     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro numquam ad, vero consequuntur aperiam cumque voluptatum pariatur veniam? Qui facilis est ipsam odio cum!",
-  profilePic: "/images/hero.png",
+  profilePic: "/images/thsr.jpg",
 };
 
 export const aboutInfo = {
--- src/app/components/Home/Home.tsx
+++ src/app/components/Home/Home.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Hero from "./Hero/Hero";
+import About from "./About/About";
+import Services from "./Services/Services";
+import Projects from "./Projects/Projects";
 
 const Home = () => {
   return (
     <div className="overflow-hidden">
       <Hero />
+      <About />
+      <Services />
+      <Projects />
     </div>
   );
 };
